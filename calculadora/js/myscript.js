28de6c2036d37adb1ad1f71bcffe13d1fefc92f7
AngelKamy <ivanvaq3@gmail.com>
2022-09-28 17:22:24 -0500

    ci:primera versión funcional

--- calculadora/js/myscript.js
+++ calculadora/js/myscript.js
@@ -1,24 +1,46 @@
 // *********** Variables para guardar los inputs **********
-let valor1 = document.getElementById("valor1");
-let valor2 = document.getElementById("valor2");
+const valor1 = document.getElementById("val1");
+const valor2 = document.getElementById("val2");
+const resultado = document.getElementById("total");
 
 // *********** Variables para guardar los inputs **********
-let addButton = document.getElementById("add");
-let subButton = document.getElementById("sub");
-let mulButton = document.getElementById("mul");
-let divButton = document.getElementById("div");
+const addButton = document.getElementById("add");
+const subButton = document.getElementById("sub");
+const mulButton = document.getElementById("mul");
+const divButton = document.getElementById("div");
 
 //console.log(valor1, valor2);
 
 // ********* Funciones ************
-const sum = (a, b) => a + b
+const sum = (a, b) => a += b
 const sub = (a, b) => a - b
 const mul = (a, b) => a * b
 const div = (a, b) => b == 0 ? window.alert("syntax error") : a/b ;
 
+
+/* Convierte los inputs en 'number' */
+// Este bloque no es de mi autoría
+const fnAdd = () => {
+    const x = parseInt(valor1.value);
+    const y = parseInt(valor2.value);
+    resultado.value = sum(x,y)
+}
+
+const fnSub = () => {
+    resultado.value = sub(valor1.value, valor2.value)
+}
+
+const fnMul = () => {
+    resultado.value = mul(valor1.value, valor2.value)
+}
+
+const fnDiv = () => {
+    resultado.value = div(valor1.value, valor2.value)
+}
+
 //*********** Eventos del sitio **********
 
-addButton.addEventListener("click", sum )
-subButton.addEventListener("click", sub)
-mulButton.addEventListener("click", mul)
-divButton.addEventListener("click", div)
+addButton.addEventListener("click", fnAdd)
+subButton.addEventListener("click", fnSub)
+mulButton.addEventListener("click", fnMul)
+divButton.addEventListener("click", fnDiv)
